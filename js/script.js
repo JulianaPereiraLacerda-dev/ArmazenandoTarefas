68ff667aec43204ff141e6da665c083dd6efa31e
JulianaPereiraLacerda-dev <julianapereiralacerda62@gmail.com>
2026-03-26 14:55:55 -0300

    Atualizei os arquivos que precisavam

--- js/script.js
+++ js/script.js
@@ -19,16 +19,69 @@ function salvarTarefas(){
 //função para mostrar as tarefas na tela
 function mostrarTarefas(){
     listaTarefas.innerHTML ="";
-    for(let i =0; i< tarefas,length; i++){
+    for(let i =0; i< tarefas.length; i++){
 
-        const li = document.creaateElement("li");
+        const li = document.createElement("li");
         li.innerText = tarefas[i]
+    
+        //botão remmover
+        const botaoRemover = document.createElement("button");
+        botaoRemover.innerText = "🗑️";
+        botaoRemover.className = "botaoRemover"
+
+        botaoRemover.addEventListener("click", () => {
+        removerTarefas(i);
+        })
+
+        li.appendChild(botaoRemover);
+        listaTarefas.appendChild(li);
     }
 }
 
+
+function removerTarefas(posicaoTarefa){
+
+    //splice -> dentro dele nos temos (posição inicial, quantidade de itens)
+    tarefas.splice(posicaoTarefa, 1);
+
+    //depois de remover chamo a função de salvar no LocalStorage
+    salvarTarefas();
+
+    //mostra as tarefas que foram atualizadas, sem as que foram removidas 
+    mostrarTarefas();
+}
 
 
 /*
     let i = o -> valor inicial da repetição vai iniciar em zero
     // i< lista.lenght -> valida se o i é menor que o tamanho da lista lenght-> verifica o tamanho da lista 
 */
+
+//função adicionar tarefas
+function adicionarTarefas(){
+    const valorTarefa = inputTarefa.value;
+
+    if(valorTarefa === ""){
+        alert("Digite uma tarefa!");
+        return;
+    }
+
+    tarefas.push(valorTarefa);// adiciona a tarefa digitada dentro do array
+    inputTarefa.value = "";
+
+    salvarTarefas();
+    mostrarTarefas();
+}
+
+//função para carregar tarefas salvas no localStorage
+function carregarTarefas(){
+    const tarefasSalvas = localStorage.getItem("tarefas");// pega as tarefas e armazena na variável
+    
+    //Se exisitir alguma coisa dentro de tarefas salvas, então converte a tarefa e mostra na tela.
+    if(tarefasSalvas){
+        tarefas = JSON.parse(tarefasSalvas); //transforma o texto em array novamente
+
+    }
+}
+
+botaoAdicionar.addEventListener("click", adicionarTarefas)
